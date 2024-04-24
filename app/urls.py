296e37b660200b0c2dc9b8278c0383d08563e1ca
Matias <matias_russo@outlook.es>
2024-04-24 16:44:00 -0300

    Agrego las urls para la entidad pets

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+    path("pets/", view=views.pets_repository, name="pets_repo"),
+    path("pets/nuevo/", view=views.pets_form, name="pets_form"),
+    path("pets/editar/<int:id>/", view=views.pets_form, name="pets_edit"),
+    path("pets/eliminar/", view=views.pets_delete, name="pets_delete"),
 ]
