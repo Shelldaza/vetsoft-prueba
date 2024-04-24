7ccd066054d7d7a87de3f19d32a9d6078d6884d6
Matias <matias_russo@outlook.es>
2024-04-24 16:22:31 -0300

    Agrego el classmodel de pet para poder actualizar la info en la DB

--- app/models.py
+++ app/models.py
@@ -77,3 +77,25 @@ class Pet(models.Model):
 
     def __str__(self):
         return self.name
+    
+    @classmethod
+    def save_pet(cls, pet_data):
+        errors = validate_pet(pet_data)
+
+        if len(errors.keys()) > 0:
+            return False, errors
+
+        Pet.objects.create(
+            name=pet_data.get("name"),
+            breed=pet_data.get("breed"),
+            birthday=pet_data.get("birthday"),
+        )
+
+        return True, None
+
+    def update_pet(self, pet_data):
+        self.name = pet_data.get("name", "") or self.name
+        self.breed = pet_data.get("breed", "") or self.breed
+        self.birthday = pet_data.get("birthday", "") or self.birthday
+
+        self.save()
