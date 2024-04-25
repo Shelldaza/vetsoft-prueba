011421069376e3725fb9879a691eea8a642fa09b
Pablo <pablodanielruival@gmail.com>
2024-04-25 12:32:42 -0300

    creo la clase MedicineForm en forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Pet
+from .models import Medicine
+
 
 class PetForm(forms.ModelForm):
     class Meta:
         model = Pet
         fields = ['name', 'breed', 'birthday']
+
+class MedicineForm(forms.ModelForm):
+    class Meta:
+        model = Medicine
+        fields = ['name', 'descripcion', 'dosis']
