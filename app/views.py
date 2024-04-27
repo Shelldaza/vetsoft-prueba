f3960f42938253cedf3b101758367bb554ee100c
Mauro Baroni <maurojavier_laplata@hotmail.com>
2024-04-27 17:21:52 -0300

    Agrego views para el product

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Client
+from .models import Client, Product
 from .models import Pet
 from .forms import PetForm
 from .models import Medicine
@@ -163,3 +163,41 @@ def provider_delete(request):
     provider = get_object_or_404(Provider, pk=int(provider_id))
     provider.delete()
     return redirect(reverse("provider_repo"))
+
+def products_repository(request):
+    products = Product.objects.all()
+    return render(request, "products/repository.html", {"products": products})
+
+
+def products_form(request, id=None):
+    if request.method == "POST":
+        product_id = request.POST.get("id", "")
+        errors = {}
+        saved = True
+
+        if product_id == "":
+            saved, errors = Product.save_product(request.POST)
+        else:
+            client = get_object_or_404(Product, pk=product_id)
+            client.update_product(request.POST)
+
+        if saved:
+            return redirect(reverse("products_repo"))
+
+        return render(
+            request, "products/form.html", {"errors": errors, "product": request.POST}
+        )
+
+    product = None
+    if id is not None:
+        product = get_object_or_404(Product, pk=id)
+
+    return render(request, "products/form.html", {"product": product})
+
+
+def products_delete(request):
+    product_id = request.POST.get("product_id")
+    product = get_object_or_404(Product, pk=int(product_id))
+    product.delete()
+
+    return redirect(reverse("products_repo"))
